10871653800895e2d277210aa4dcc2a65ff06699
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-05 15:31:21 +0300

    learn middleware

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '');
+    next();
+});
+
+//app.use((req, res, next) => {
+//    res.render('maintenance.hbs');
+//});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
